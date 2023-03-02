0aa87593a0ca9ab9c5a2a14e9e8e10783a707e0d
V4nd3rs0n <vandersondesouza.42@gmail.com>
2023-03-02 11:56:18 -0300

    oi

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>ARENA PHYREXIANA</h1>
+      <h2>"A audiência de um, com a malicia de milhares" </h2>
+    </div>
+  );
+}
+
+export default App;
